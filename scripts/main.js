9608c5981a56c05a01d0547b0c1a1c4a98609377
Oamogetswe Makgopela <opmakgopela@gmail.com>
2024-06-05 16:40:03 +0200

    Add files via upload

---
+++ scripts/main.js
@@ -0,0 +1,43 @@
+const scrollRevealOption = {
+	distance: "50px",
+	origin: "bottom",
+	duration: 1000,
+};
+
+/**Animation for letter O*/
+ScrollReveal().reveal(".container .letter-o", {
+	duration: 1000,
+	delay: 1000,
+});
+/**Animation for centre image*/
+ScrollReveal().reveal(".container img", {
+	duration: 1000,
+	delay: 1500,
+});
+
+/**Animation for explore button*/
+ScrollReveal().reveal(".container .explore", {
+	duration: 1000,
+	delay: 2000,
+});
+/**Animation for interests button*/
+ScrollReveal().reveal(".container .interests", {
+	duration: 1000,
+	delay: 3000,
+});
+/**Animation for print button*/
+ScrollReveal().reveal(".container .print", {
+	duration: 1000,
+	delay: 4000,
+});
+
+ScrollReveal().reveal(".container h5", {
+	duration: 1000,
+	interval: 500,
+	delay: 3000,
+});
+ScrollReveal().reveal(".footer p", {
+	duration: 1000,
+	delay: 6000,
+});
+
